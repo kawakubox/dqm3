def0108d8976b934e7f1365664cbbf76a3dac96b
kawakubox <shigefumi.kawakubo@gmail.com>
2023-12-31 14:05:57 +0900

    feat(ViewComponent): Add GrowthTableRow component

--- app/models/monster_growth.rb
+++ app/models/monster_growth.rb
@@ -8,4 +8,12 @@ class MonsterGrowth < ApplicationRecord
   validates :defence, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
   validates :speed, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
   validates :intelligence, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
+  def self.table_items(monster:, to: 99)
+    range = 1..to
+    growths = MonsterGrowth.where(monster: monster, level: range)
+    range.map do |i|
+      growths.find_by(level: i) || MonsterGrowth.new(monster: monster, level: i)
+    end
+  end
 end
